f3eb1c2ee245ae4dcad3a6f4f8f18c62c55ca35e
SubirChDey <subirdey93@gmailcom>
2024-12-06 21:20:31 -0800

    first commit

---
+++ js.js
@@ -0,0 +1,9 @@
+document.getElementById('noakhaliDonateBtn').addEventListener('click', function(event){
+    event.preventDefault();
+    const noakhaliDonateMoney = document.getElementById('noakhaliDonateInput').value;  
+    const donateMoneyNumber = parseFloat(NoakhaliDonateMoney);
+      const noakhaliDonateBalance = getElementById('noakhaliBalance').innerText;
+      const noakhaliDonateBalanceNumber = parseFloat(noakhaliDonateBalance);
+      
+      
+})
