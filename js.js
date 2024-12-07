55858ffafef381fcfae6696ae7ff773af7846fc6
SubirChDey <subirdey93@gmailcom>
2024-12-07 04:00:00 -0800

    javascript added with 1st card

--- js.js
+++ js.js
@@ -1,9 +1,26 @@
-document.getElementById('noakhali-Donate-Btn').addEventListener('click', function(event){
-    event.preventDefault();
-    const donateMoney = document.getElementById('noakhali-donate-input').value;  
-    const donateMoneyNumber = parseFloat(donateMoney);
-      const noakhaliBalance = document.getElementById('noakhali-Balance').innerText;
-      const noakhaliBalanceNumber = parseFloat(noakhaliBalance);
+document.getElementById('noakhali-Donate-Btn').addEventListener('click', function (event) {
+  event.preventDefault();
+  const donateMoney = document.getElementById('noakhali-donate-input').value;
 
-      const nokhaliNewBalance = noakhaliBalanceNumber + donateMoneyNumber;
+  const noakhaliBalance = document.getElementById('noakhali-Balance').innerText;
+
+  const mainBalance = document.getElementById('main-balance').innerText;
+  const donateMoneyNumber = parseFloat(donateMoney);
+  const noakhaliBalanceNumber = parseFloat(noakhaliBalance);
+  const mainBalanceNumber = parseFloat(mainBalance);
+
+  if (donateMoneyNumber > 0) {
+
+    const noakhaliNewBalance = noakhaliBalanceNumber + donateMoneyNumber;
+    const mainNewBalance = mainBalanceNumber - donateMoneyNumber;
+
+    document.getElementById('noakhali-Balance').innerText = noakhaliNewBalance;
+    document.getElementById('main-balance').innerText = mainNewBalance;
+
+
+  }
+  else {
+    alert('Go back')
+
+  }
 })
